8b6f56671d6112939d000cec9d512e08a401918d
lamis-menghour <lamis-menghour>
2021-11-16 12:54:39 +0100

    problem set 1 done

--- pset1/psa1.py
+++ pset1/psa1.py
@@ -1,1 +1,32 @@
 # this is partie A
+total_cost= float(input("Enter the cost of your dream house :"))
+portion_down_payment =0.25*total_cost # 25% دفعة أولى
+print("The portion down payment is" , portion_down_payment)
+annual_salary =float(input("Enter your annual salary:"))
+portion_saved =float(input("Enter the percent of your salary to save, as a decimal:"))
+semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal:"))
+
+current_savings =0
+r =0.04 #4%
+monthly_saving = (annual_salary/12 ) *portion_saved 
+
+number_of_months= 1
+while current_savings < portion_down_payment :
+	number_of_months = number_of_months +1 
+	current_savings = current_savings + monthly_saving 
+	current_savings = current_savings*r/12 + current_savings 
+
+print('Current savings ', current_savings )
+print('Number of months ', number_of_months)
+
+# Test Case 1
+# Enter your annual salary:​ 120000
+# Enter the percent of your salary to save, as a decimal: ​ . 10
+# Enter the cost of your dream home:​ 1000000
+# Number of months:​ 183
+
+#Test Case 2
+# Enter your annual salary:​ 80000
+# Enter the percent of your salary to save, as a decimal: ​ . 15
+# Enter the cost of your dream home:​ 500000
+# Number of months:​ 105
---
+++ pset1/psb1.py
@@ -0,0 +1,42 @@
+# this is partie B
+total_cost= float(input("Enter the cost of your dream house :"))
+portion_down_payment =0.25*total_cost # 25% دفعة أولى
+print("The portion down payment is" , portion_down_payment)
+annual_salary =float(input("Enter your annual salary:"))
+portion_saved =float(input("Enter the percent of your salary to save, as a decimal:"))
+semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal:"))
+
+current_savings =0
+r =0.04 #4%
+
+number_of_months= 1
+while current_savings < portion_down_payment :
+	number_of_months = number_of_months +1 
+	current_savings = current_savings + (annual_salary/12 ) *portion_saved 
+	current_savings = current_savings*r/12 + current_savings 
+	if number_of_months % 6 == 0 :
+		annual_salary = annual_salary + annual_salary*semi_annual_raise
+print('Number of months ', number_of_months)
+
+# Test Case 1   
+# Enter your starting annual salary:​ 120000 
+# Enter the percent of your salary to save, as a decimal: .​ 05 
+# Enter the cost of your dream home: ​500000 
+# Enter the semi­annual raise, as a decimal:​ .03 
+# Number of months:​ 142 
+
+ 
+# Test Case 2   
+# Enter your starting annual salary:​ 80000 
+# Enter the percent of your salary to save, as a decimal: .​ 1 
+# Enter the cost of your dream home: ​800000 
+# Enter the semi­annual raise, as a decimal:​ .03 
+# Number of months:​ 159 
+ 
+ 
+# Test Case 3 
+# Enter your starting annual salary:​ 75000 
+# Enter the percent of your salary to save, as a decimal: .​ 05 
+# Enter the cost of your dream home:​ 1500000 
+# Enter the semi­annual raise, as a decimal:​ .05 
+# Number of months:​ 261 
